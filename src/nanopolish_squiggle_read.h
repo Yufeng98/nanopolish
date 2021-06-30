6a58b117e37c5e5524613facea6014308364f8c8
Yufeng98 <guyufeng0217@163.com>
2021-06-30 16:24:11 +0800

    approximation 14 18

--- src/nanopolish_squiggle_read.h
+++ src/nanopolish_squiggle_read.h
@@ -30,7 +30,7 @@ using namespace flx;
 
 using namespace Fixed64;
 using namespace numeric;
-typedef Fixed<16, 16> fixed;
+typedef Fixed<14, 18> fixed;
 typedef Fixed<20, 12> fixed_long;
 
 enum PoreType
