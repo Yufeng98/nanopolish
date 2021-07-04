787c5a6815bd46c252fc2c544661a5d2a0a62637
Yufeng98 <guyufeng0217@163.com>
2021-07-04 16:47:20 +0800

    data process

--- data_process.py
+++ data_process.py
@@ -122,11 +122,10 @@ def check_error_for_all_reads(data_type):
             diff = abs(float(dic[key][0][1])-float(dic[key][1][1]))
             # print(key[-36:])
             if key[-36:] in dic_read.keys(): 
-                print(key[-36:], diff)
                 dic_read[key[-36:]] += [diff]
             else: 
-                print(key[-36:], diff)
                 dic_read[key[-36:]] = [diff]
+            print(dic_read[key[-36:]])
             if key[-36:] in dic_cnt.keys(): dic_cnt[key[-36:]] += 1
             else: dic_cnt[key[-36:]] = 1
             diff_ratio += 1
